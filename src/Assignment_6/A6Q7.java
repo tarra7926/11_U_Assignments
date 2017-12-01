d20c7226ebe8b9594f35fcded9f8ce9352506398
tarra7926 <tarra7926@HRH-IC0027323.wrdsb.ca>
2017-12-01 14:02:56 -0500

    Done assignment 6, Started 7

--- src/Assignment_6/A6Q7.java
+++ src/Assignment_6/A6Q7.java
@@ -3,7 +3,7 @@ package Assignment_6;
 import java.util.Scanner;
 
 /**
- * 
+ * Find all the prime numbers between 2 and 1000
  * @author tarra7926
  */
 public class A6Q7 {
@@ -22,23 +22,25 @@ public class A6Q7 {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = i + 2;
         }
-        //create a int variable
+        //create a int variable called currentPrime  which is equal to 0
         int currentPrime = 0;
-        
+        //loop the following if the currentPrime number is less then half the length of the nums array
         while (currentPrime < nums.length/2) {
+            //fill the nonPrimeNums arrray with non prime numbers
             for (int i = currentPrime + nums[currentPrime]; i < nums.length; i = i + nums[currentPrime]) {
                 nonPrimeNums[i] = nums[i];
             }
+            //find the next spot in the nonPrimeNums array that is a prime number and make the currentPrime variable be equal to that spot number
             for (int i = currentPrime + 1; i < nums.length; i = i + 1) {
                 if (nonPrimeNums[i] == 0) {
                     currentPrime = i;
                     break;
                 }
             }
-  
         }
+        //create a int variable called numOdPrime
         int numOfPrime = 0;
-        
+        //find how many prime numbers there are int the nonPrimeNums array
         for (int i = 0; i < nums.length; i = i + 1) {
                 if (nonPrimeNums[i] == 0) {
                     numOfPrime =  numOfPrime + 1;
@@ -46,16 +48,19 @@ public class A6Q7 {
                 }
             }
         
-        
+        //create a int array callled prime nums which is as long as the numOfPrime variable
         int[] primeNums = new int[numOfPrime];
-        
+        //create a int variable called primeNumsSpot which is equal to -1
         int primeNumsSpot = -1;
+        //fill the primeNums array with prime numbers
         for (int i = 0; i < nums.length; i = i + 1) {
             if (nonPrimeNums[i] == 0) {
                 primeNumsSpot = primeNumsSpot + 1;
                 primeNums[primeNumsSpot] = i + 2;
             }
         }
+        System.out.print("These are the prime numbers bewteen 2 and 1000: ");
+        //output all prime numbers between 2 and 1000
         for (int i = 0; i < primeNums.length; i = i + 1) {
             System.out.print(primeNums[i] + " ");
         }
